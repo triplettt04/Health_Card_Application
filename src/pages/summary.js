fbae02c27d9d725f83d3bcb26c4dcef445b4420c
Marcus Hum <marcus.hum@ontario.ca>
2019-02-12 11:15:23 -0500

    summary page completed, option buttons added, various fixes

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -31,6 +31,42 @@ class Summary extends React.Component {
         </div>
         <div className="form-wrapper">
           <h1 className="primary-header">Here's how applying online works</h1>
+          <div className="summary-content">
+            <div className="summary-step">
+              <img src={require("../assets/upload-graphic.svg")} />
+              <h2 className="sub-header">Upload your documents</h2>
+              <p className="caption">
+                Submit relevant documents needed to complete the application
+              </p>
+            </div>
+            <div className="spacer" />
+            <div className="summary-step">
+              <img src={require("../assets/form-graphic.svg")} />
+              <h2 className="sub-header">Fill out the form</h2>
+              <p className="caption">
+                Complete the online application form with your personal
+                information
+              </p>
+            </div>
+            <div className="spacer" />
+            <div className="summary-step">
+              <img src={require("../assets/license-graphic.svg")} />
+              <h2 className="sub-header">
+                Wait for your health card in the mail
+              </h2>
+              <p className="caption">
+                If you completed the steps above correctly, then your Ontario
+                health card will be mailed to you soon!
+              </p>
+            </div>
+          </div>
+          <div className="btn-container btn-center">
+            <input
+              type="submit"
+              value="Get started"
+              className="btn btn-general btn-wide btn-shadow"
+            />
+          </div>
         </div>
       </form>
     );
