546c9592106d7891e1247dd1427b9294dfd0d297
Matthew Stokes <matthew.stokes@ontario.ca>
2019-02-13 10:47:47 -0500

    Added base autocomplete

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,19 @@ const constants = {
   navTopName: "Home",
   militaryAddresses: [
     {
+      label: "Carp Almonte",
+      street: "7R6X+Q3 Hardwood Plains",
+      city: "Ottawa",
+      postalCode: "K0A1A0"
+    },
+    {
+      label: "Armstrong",
+      street: "8X3Q+QH Armstrong Station",
+      city: "Unorganized Thunder Bay District",
+      postalCode: ""
+    },
+    {
+      label: "Test3",
       street: "123 Sesame street",
       city: "PleasantVille",
       postalCode: "A1A1A1"
