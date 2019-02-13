88caa8bd235d8fa90563ee03f32d11ae1dd8ff8d
Matthew Stokes <matthew.stokes@ontario.ca>
2019-02-13 11:15:56 -0500

    Added all military bases

--- src/constants.js
+++ src/constants.js
@@ -3,22 +3,100 @@ const constants = {
   navTopName: "Home",
   militaryAddresses: [
     {
-      label: "Carp Almonte",
-      street: "7R6X+Q3 Hardwood Plains",
-      city: "Ottawa",
-      postalCode: "K0A1A0"
-    },
-    {
       label: "Armstrong",
       street: "8X3Q+QH Armstrong Station",
       city: "Unorganized Thunder Bay District",
       postalCode: ""
     },
     {
-      label: "Test3",
-      street: "123 Sesame street",
-      city: "PleasantVille",
-      postalCode: "A1A1A1"
+      label: "Borden"
+    },
+    {
+      label: "Bytown"
+    },
+    {
+      label: "Camp Ipperwash"
+    },
+    {
+      label: "Carp"
+    },
+    {
+      label: "Carp Almonte",
+      street: "7R6X+Q3 Hardwood Plains",
+      city: "Ottawa",
+      postalCode: "K0A1A0"
+    },
+    {
+      label: "Carp Dunrobin"
+    },
+    {
+      label: "Carp Richardson"
+    },
+    {
+      label: "Clinton"
+    },
+    {
+      label: "Cobourg"
+    },
+    {
+      label: "Falconbridge"
+    },
+    {
+      label: "Foymount"
+    },
+    {
+      label: "Kingston"
+    },
+    {
+      label: "Leitrim"
+    },
+    {
+      label: "London"
+    },
+    {
+      label: "Lowther"
+    },
+    {
+      label: "Meaford"
+    },
+    {
+      label: "Moosonee"
+    },
+    {
+      label: "Mountain View"
+    },
+    {
+      label: "North Bay"
+    },
+    {
+      label: "Ottawa"
+    },
+    {
+      label: "Petawawa"
+    },
+    {
+      label: "Picton"
+    },
+    {
+      label: "Ramore"
+    },
+    {
+      label: "Sioux Lookout"
+    },
+    {
+      label: "Toronto"
+    },
+    {
+      label: "Trenton",
+      street: "1000 Stn Forces",
+      city: "Astra",
+      postalCode: "K0K3W0"
+    },
+    {
+      label: "Uplands"
+    },
+    {
+      label: "York"
     }
   ]
 };
