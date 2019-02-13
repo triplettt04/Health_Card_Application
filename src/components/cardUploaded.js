52a47215cb0d962397aae0b142980d18fb419951
Marcus Hum <marcus.hum@ontario.ca>
2019-02-13 14:25:14 -0500

    Upload pages styled, content still needed for several

---
+++ src/components/cardUploaded.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CardUploaded extends React.Component {
+  render() {
+    return <div className="card-uploaded">{this.props.content}</div>;
+  }
+}
+
+export default CardUploaded;
--- src/pages/uploadRes.js
+++ src/pages/uploadRes.js
@@ -1,6 +1,22 @@
 import React from "react";
 import constants from "../constants";
 import Card from "../components/card";
+import CardUploaded from "../components/cardUploaded";
+import iconTrash from "../assets/icon-trash.svg";
+import postingLetter from "../assets/posting-letter.png";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "80px",
+    left: "25px",
+    right: "25px",
+    padding: "0",
+    bottom: "unset"
+  }
+};
+
+Modal.defaultStyles.overlay.backgroundColor = "rgb(58, 58, 58, 0.75)";
 
 class UploadRes extends React.Component {
   constructor(props) {
@@ -10,12 +26,31 @@ class UploadRes extends React.Component {
         ? "Uploaded"
         : "Not completed";
     this.state = {
-      status: status
+      status: status,
+      modalIsOpen: false
     };
 
     this.back = this.back.bind(this);
     this.next = this.next.bind(this);
     this.takePic = this.takePic.bind(this);
+
+    //Modal
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = "#000";
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   takePic() {
@@ -49,10 +84,26 @@ class UploadRes extends React.Component {
   render() {
     let content = (
       <div>
-        <div>Upload your residence proof</div>
-        <div>Current status: {this.state.status}</div>
+        <div className="progress-indicator">2 / 15</div>
+        <h2 className="sub-header">UPLOAD RESIDENCY</h2>
+        <p className="caption">
+          When relocating to Ontario, posting messages show your intent to move
+          into the province.
+        </p>
       </div>
     );
+    let uploaded = (
+      <div>
+        <a href="#" className="view-link" onClick={this.openModal}>
+          View
+        </a>
+        <div className="file-name">military_posting1.jpeg</div>
+        <button className="delete-icon">
+          <img src={iconTrash} />
+        </button>
+      </div>
+    );
+
     return (
       <form onSubmit={event => this.next(event)}>
         <nav className="navbar ontario-header-container">
@@ -65,12 +116,36 @@ class UploadRes extends React.Component {
         </nav>
         <div className="form-wrapper">
           <Card content={content} />
-          <button
-            className={constants.buttonClasses}
-            onClick={() => this.takePic()}
-          >
-            Take a picture
-          </button>
+          <CardUploaded content={uploaded} />
+          <div className="links-container">
+            <div class="icon-link-container">
+              <a
+                href="#"
+                className="icon-link icon-photo"
+                onClick={() => this.takePic()}
+              >
+                Take a photo
+              </a>
+            </div>
+            <div class="icon-link-container">
+              <a
+                href="#"
+                className="icon-link icon-folder"
+                onClick={() => this.takePic()}
+              >
+                Browse files
+              </a>
+            </div>
+            <div class="icon-link-container">
+              <a
+                href="#"
+                className="icon-link icon-gallery"
+                onClick={() => this.takePic()}
+              >
+                View gallery
+              </a>
+            </div>
+          </div>
           <div className="btn-container">
             <button
               className="btn btn-general btn-invert"
@@ -85,6 +160,26 @@ class UploadRes extends React.Component {
             />
           </div>
         </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="upload modal"
+        >
+          <div className="modal-head">
+            <div
+              className="file-name"
+              ref={subtitle => (this.subtitle = subtitle)}
+            >
+              military_posting1.jpeg
+            </div>
+            <a className="modal-close" onClick={this.closeModal} />
+          </div>
+          <div className="modal-image">
+            <img src={postingLetter} />
+          </div>
+        </Modal>
       </form>
     );
   }
