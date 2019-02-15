e2cc8f65225df873835d41da45514d7d4a7d331c
Marcus Hum <marcus.hum@ontario.ca>
2019-02-15 13:51:08 -0500

    added desktop compatibility, edited not-found page, added more focus tech

--- src/pages/address.js
+++ src/pages/address.js
@@ -1,6 +1,7 @@
 import React from "react";
 import constants from "../constants";
 import Card from "../components/card";
+import Nav from "../components/nav";
 
 class Address extends React.Component {
   constructor(props) {
@@ -109,14 +110,7 @@ class Address extends React.Component {
 
     return (
       <form onSubmit={event => this.next(event)}>
-        <nav className="navbar ontario-header-container">
-          <a className="brand" href="#">
-            OHIP application
-          </a>
-          <a className="french-toggle" href="#">
-            FR
-          </a>
-        </nav>
+        <Nav />
         <div className="form-wrapper">
           <Card content={content} />
           <label className="form-label">
